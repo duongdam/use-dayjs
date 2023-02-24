48d23ac63fa69b8b17fc21e95ef11bf93e7390e5
duongdampa <duong.dam@classfunc.com>
2023-02-24 16:25:45 +0700

    update version

--- calculator/index.cjs.js
+++ calculator/index.cjs.js
@@ -21,10 +21,6 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-    // If the importer is in node compatibility mode or this is not an ESM
-    // file that has been converted to a CommonJS file using a Babel-
-    // compatible transform (i.e. "__esModule" has not been set), then set
-    // "default" to the CommonJS "module.exports" for node compatibility.
     isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", {value: mod, enumerable: true}) : target,
     mod
 ));
@@ -173,7 +169,7 @@ const djsGet = (aTime, type = "millisecond") => {
 };
 const djsSet = (aTime, config) => {
   if (!aTime)
-    aTime = /* @__PURE__ */ new Date();
+    aTime = new Date();
   return (0, import_dayjs.default)(aTime).set("year", (config == null ? void 0 : config.year) || djsGet(aTime, "year")).set("month", (config == null ? void 0 : config.month) || djsGet(aTime, "month")).set("date", (config == null ? void 0 : config.date) || djsGet(aTime, "date")).set("day", (config == null ? void 0 : config.day) || djsGet(aTime, "day")).set("hour", (config == null ? void 0 : config.hour) || djsGet(aTime, "hour")).set("minute", (config == null ? void 0 : config.minute) || djsGet(aTime, "minute")).set("second", (config == null ? void 0 : config.second) || djsGet(aTime, "second")).set("millisecond", (config == null ? void 0 : config.millisecond) || djsGet(aTime, "millisecond"));
 };
 const djsDefault = (aTime, valueOf = false) => {
--- locale/index.cjs.js
+++ locale/index.cjs.js
@@ -7,7 +7,7 @@ var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
   for (var name in all)
-      __defProp(target, name, {get: all[name], enumerable: true});
+    __defProp(target, name, {get: all[name], enumerable: true});
 };
 var __copyProps = (to, from, except, desc) => {
     if (from && typeof from === "object" || typeof from === "function") {
@@ -21,10 +21,6 @@ var __copyProps = (to, from, except, desc) => {
     return to;
 };
 var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-    // If the importer is in node compatibility mode or this is not an ESM
-    // file that has been converted to a CommonJS file using a Babel-
-    // compatible transform (i.e. "__esModule" has not been set), then set
-    // "default" to the CommonJS "module.exports" for node compatibility.
     isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", {value: mod, enumerable: true}) : target,
     mod
 ));
